7571eebc7f02ad0a06d4e5b9d6173d2ee976421f
MrLegoTL <martingarciacurto@gmail.com>
2024-01-24 23:54:35 +0100

    Mecanicas
    -Movimiento de salchicha.
    -Script para cuando toca plataforma de salto.
    - Script para cuento toca plataforma de impulso

--- Save The Sausage/Assets/Scripts/Player.cs
+++ Save The Sausage/Assets/Scripts/Player.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    
     public float speed = 5f;
+    public bool grounded = false;
+    [SerializeField]
+    private float timeImpulse = 0f;
+    [SerializeField]
+    private float currentImpulse;
+    private Rigidbody2D rb;
+    
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         
     }
 
@@ -15,10 +24,51 @@ public class Player : MonoBehaviour
     void Update()
     {
         Move();
+        ImpulseDuration();
     }
 
     public void Move()
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if (grounded)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Grounded")
+        {
+            grounded = true;
+            
+        }
+        else
+        {
+            grounded = false;
+            
+        }
+    }
+
+    public void AddImpulse(float impulse)
+    {
+        currentImpulse = impulse;
+        timeImpulse = 1f;
+    }
+    void ImpulseDuration()
+    {
+        if (timeImpulse > 0)
+        {
+            rb.velocity = new Vector2(currentImpulse, rb.velocity.y);
+            timeImpulse -= Time.deltaTime;
+            Debug.Log("Impuso cogido");
+        }
+        else if (timeImpulse <= 0)
+        {
+            currentImpulse = 0f;
+            Debug.Log("No hay impulso");
+        }
+    }
+
+    
 }
