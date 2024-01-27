6d86fd2c351f647cdaf4bcfa1220baf76a5cb6d8
Martin <martingarciacurto@gmail.com>
2024-01-27 19:56:23 +0100

    Plataformas y Obstaculos

--- Save The Sausage/Assets/Scripts/JumpPlatform.cs
+++ Save The Sausage/Assets/Scripts/JumpPlatform.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class JumpPlatform : MonoBehaviour
 {
     public float jumpForce = 10f;
+    Animator anim;
 
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+
             Rigidbody2D rbPlayer = collision.gameObject.GetComponent<Rigidbody2D>();
             rbPlayer.velocity = new Vector2(rbPlayer.velocity.x, jumpForce);
+            anim.SetBool("Jump", true);
         }
     }
 }
