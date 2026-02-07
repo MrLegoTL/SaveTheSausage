2fc1247bcbe172ab845d283d32e30fcd899a59cf
Martín <151843221+MrLegoTL@users.noreply.github.com>
2026-02-08 00:28:19 +0100

    Empiezo de port a Android

--- Save The Sausage/Assets/Scripts/PickPlatform.cs
+++ Save The Sausage/Assets/Scripts/PickPlatform.cs
@@ -12,19 +12,21 @@ public class PickPlatform : MonoBehaviour
     {
         if (selectedRigidbody)
         {
-            Vector3 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - selectedRigidbody.transform.position;
+            Vector3 dir = Camera.main.ScreenToWorldPoint(GetInputPosition()) - selectedRigidbody.transform.position;
             selectedRigidbody.velocity = dir * force * Time.fixedDeltaTime;
         }
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        //Detecta el inicio del toque/clic
+        if (GetInputDown())
         {
             selectedRigidbody = GetRigidBodyFromMouseClick();
         }
 
-        if(Input.GetMouseButtonUp(0))
+        //Detecta cuando se suelta el toque/clic
+        if(GetInputUp())
         {
             if(selectedRigidbody != null)
             {
@@ -40,9 +42,33 @@ public class PickPlatform : MonoBehaviour
         }
     }
 
+
+   //Detecta si se presiono con raton o tactil
+    private bool GetInputDown()
+    {
+        return Input.GetMouseButtonDown(0) || (Input.touchCount>0 && Input.GetTouch(0).phase == TouchPhase.Began);
+    }
+
+    //Detecta si se dejo de presiona con raton o tactil.
+    private bool GetInputUp()
+    {
+        return Input.GetMouseButtonUp(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended);
+    }
+
+    //Obtiene la posicion del Input tanto para raton como tactil
+    private Vector3 GetInputPosition()
+    {
+        if (Input.touchCount > 0)
+        {
+            return Input.GetTouch(0).position;
+
+        }
+        return Input.mousePosition;
+    }
+
     Rigidbody2D GetRigidBodyFromMouseClick()
     {
-         Vector2  clickPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+         Vector2  clickPoint = Camera.main.ScreenToWorldPoint(GetInputPosition());
         RaycastHit2D hit =Physics2D.Raycast(clickPoint, Vector2.zero);
 
         if (hit)
